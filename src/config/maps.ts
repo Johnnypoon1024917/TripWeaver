0a77f1b12f261631153630e96a0f06f5526343cb
Johnny <cypoon54@gmail.com>
2025-11-28 19:29:46 +0800

    Commit for testing ui

--- src/config/maps.ts
+++ src/config/maps.ts
@@ -1,7 +1,7 @@
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBmFCy71VLlgNcbvEQNu2azuSM5flgArE4';
 
 // Mapbox Access Token - replace with your own
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoieW91cnVzZXJuYW1lIiwiYSI6ImNsdXJzdG9uMDAwdHEybG8zcXV0bHd1YzgifQ.YourAccessTokenHere';
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoieW91cnVzZXJuYW1lIiwiYSI6ImNsdXJzdG9uMDAwdHEybG8zcXV0bHd1YzgifQ.69Q40v5N7kMCjm7AhjD1-w';
 
 export const mapStyle = [
   {
